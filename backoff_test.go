1e2a00919d5a4bf941027e5d22cf669acd920cd8
vthiery <vjmthiery@gmail.com>
2020-08-24 20:50:46 +0200

    Improve tests

--- backoff_test.go
+++ backoff_test.go
@@ -7,34 +7,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConstantBackoffNextTime(t *testing.T) {
+func TestConstantBackoffNext(t *testing.T) {
 	backoff := NewConstantBackoff(100*time.Millisecond, 50*time.Millisecond)
 
-	assert.True(t, 0*time.Millisecond <= backoff.Next(0))
-	assert.True(t, 100*time.Millisecond <= backoff.Next(1))
+	assert.Equal(t, 0*time.Millisecond, backoff.Next(0))
+
+	for i := 1; i < 100; i++ {
+		n := backoff.Next(i)
+		assert.True(t, 100*time.Millisecond <= n)
+		assert.True(t, n <= 150*time.Millisecond)
+	}
 }
 
-func TestExponentialBackoffNextTime(t *testing.T) {
+func TestExponentialBackoffNext(t *testing.T) {
 	backoff := NewExponentialBackoff(2*time.Millisecond, 10*time.Millisecond, 2.0, 1*time.Millisecond)
 
-	assert.True(t, 0*time.Millisecond <= backoff.Next(0))
-	assert.True(t, 4*time.Millisecond <= backoff.Next(1))
-}
+	n := backoff.Next(0)
+	assert.Equal(t, 0*time.Millisecond, n)
 
-func TestExponentialBackoffMaxTimeoutCrossed(t *testing.T) {
-	backoff := NewExponentialBackoff(2*time.Millisecond, 9*time.Millisecond, 2.0, 1*time.Millisecond)
+	n = backoff.Next(1)
+	assert.True(t, 4*time.Millisecond <= n)
+	assert.True(t, n <= 5*time.Millisecond)
 
-	assert.True(t, 9*time.Millisecond <= backoff.Next(3))
-}
+	n = backoff.Next(2)
+	assert.True(t, 6*time.Millisecond <= n)
+	assert.True(t, n <= 7*time.Millisecond)
 
-func TestExponentialBackoffMaxTimeoutReached(t *testing.T) {
-	backoff := NewExponentialBackoff(2*time.Millisecond, 10*time.Millisecond, 2.0, 1*time.Millisecond)
-
-	assert.True(t, 10*time.Millisecond <= backoff.Next(3))
-}
-
-func TestExponentialBackoffJitter(t *testing.T) {
-	backoff := NewExponentialBackoff(2*time.Millisecond, 10*time.Millisecond, 2.0, 2*time.Millisecond)
-
-	assert.True(t, 4*time.Millisecond <= backoff.Next(1))
+	// Next times, the maximum wait time will be reached
+	for i := 3; i < 100; i++ {
+		assert.Equal(t, 10*time.Millisecond, backoff.Next(i))
+	}
 }
